2e60bf0cc5eece6acad525e1636c42e74b81186d
Seongeun Jeong <jeongse@gmail.com>
2020-07-21 12:24:51 -0700

    calc_average_wind_direction added

--- R/wind.R
+++ R/wind.R
@@ -104,3 +104,40 @@ uv_to_meteor_wdir <- function (u,v) {
     met_direction = (180 / pi) * atan (u / v) + ANG
     return (met_direction)
 }
+
+
+#' Calculate the average wind direction.
+#'
+#' @param wdir in degrees (0 - 360) where 0 or 360 is from the north.
+#'
+#' @return
+#' @export calc_average_wind_direction
+#' @import circular
+#' @import dplyr
+#' @examples
+calc_average_wind_direction <- function (wdir) {
+    #> wdir = c(355, 5, 15)
+    #> mean(circular(wdir, units = "degrees"))
+    #Circular Data:
+    #    Type = angles
+    #Units = degrees
+    #Template = none
+    #Modulo = asis
+    #Zero = 0
+    #Rotation = counter
+    #[1] 5
+
+    # The following all works!
+    #wdir = 1:180
+    #wdir = 90:270
+    #wdir = c(270:360, 1:90)
+    mean_wdir = mean(circular(wdir, units = "degrees")) %>% as.numeric()
+    if (is.na (mean_wdir)) {
+        #mean_wdir = NA # theoretically it could be 0
+        stop ('winds are from opposite directions for each other')
+    } else if (mean_wdir < 0) {
+        mean_wdir = mean_wdir + 360
+    }
+    #print (mean_wdir)
+    return (mean_wdir)
+}
